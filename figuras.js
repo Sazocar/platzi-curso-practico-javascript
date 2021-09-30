290e88c2254251c63217016add1b22f6210b41ed
Simón A Azócar <saimn.azocar@gmail.com>
2021-09-29 23:20:56 -0400

    Agregado estilos y implementadas las funciones de js con HTML/

--- figuras.js
+++ figuras.js
@@ -18,12 +18,14 @@ console.groupEnd();
 console.group('Triángulos');
 
 function perimetroTriangulo(lado, lado2, base) {
-    return lado + lado2 + base;
+    resultado = lado + lado2 + base;
+    return resultado;
 }
 
 function areaTriangulo(base, altura) {
     return (base * altura) / 2;
 }
+
 console.groupEnd();
 
 // Código de Circulo:
@@ -44,3 +46,73 @@ function areaCirculo(radio) {
     return parseFloat(resultado.toFixed(2));
 }
 console.groupEnd();
+
+
+// Aquí interactuamos con el HTML
+
+function calcularPerimetroCuadrado() {
+    let input = document.getElementById('InputCuadrado');
+    let value = input.value;
+
+    let perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+
+function calcularAreaCuadrado() {
+    let input = document.getElementById('InputCuadrado');
+    let value = input.value;
+
+    let area = areaCuadrado(value);
+    alert(area);
+}
+
+
+function calcularPerimetroTriangulo() {
+    let ladoA = parseInt(document.getElementById('ladoATriangulo').value);
+    let ladoB = parseInt(document.getElementById('ladoBTriangulo').value);
+    let base = parseInt(document.getElementById('baseTriangulo').value);
+
+    perimetro = perimetroTriangulo(ladoA, ladoB, base);
+    alert(perimetro);
+}
+
+
+function calcularAreaTriangulo() {
+    let base = parseInt(document.getElementById('baseTriangulo').value);
+    let altura = parseInt(document.getElementById('alturaTriangulo').value);
+
+    area = areaTriangulo(base, altura);
+    alert(area);
+}
+
+function calcularAlturaTriangulo() {
+    let ladoA = parseInt(document.getElementById('ladoATriangulo').value);
+    let ladoB = parseInt(document.getElementById('ladoBTriangulo').value);
+    let base = parseInt(document.getElementById('baseTriangulo').value);
+
+    if (ladoA === ladoB && ladoA != base) {
+        let altura = Math.sqrt(ladoA**2 - (ladoB**2 / 4));
+        altura = parseFloat(altura.toFixed(2));
+        alert(`La altura del triangulo isoceles es: ${altura}`);
+        document.getElementById('alturaTriangulo').value = altura;
+    } else {
+        alert('Este triangulo NO es isosceles.');
+    }
+}
+
+
+function calcularPerimetroCirculo() {
+    let radio = document.getElementById('radioCirculo').value;
+
+    perimetro = perimetroCirculo(radio);
+    alert(perimetro);
+}
+
+
+function calcularAreaCirculo() {
+    let radio = document.getElementById('radioCirculo').value;
+
+    area = areaCirculo(radio);
+    alert(area);
+}
