0794876ba9ff27eb741027bd56223d5f2ef87a47
Simón A Azócar <saimn.azocar@gmail.com>
2021-10-04 18:47:49 -0400

    Añadido el index

--- promedio.js
+++ promedio.js
@@ -33,8 +33,8 @@ function agregarNumeroPromedio() {
 }
 
 function mostrarResultadoPromedio() {
-  let resultado_input = document.getElementById('ResultadoInputPromedio');
-  resultado_input.innerText = 'El promedio es: ' + calcularMediaAritmetica(numberListPromedio);
+  let resultado_input_promedio = document.getElementById('ResultadoInputPromedio');
+  resultado_input_promedio.innerText = 'El promedio es: ' + calcularMediaAritmetica(numberListPromedio);
 }
 
 function resetListPromedio() {
@@ -45,4 +45,70 @@ function resetListPromedio() {
   let lista_de_numeros = document.getElementById('NumberListPromedio');
   lista_de_numeros.innerText = numberListPromedio;
   document.getElementById('ResultadoInputPromedio').innerText = '';
+}
+
+// Codigo de la Mediana
+
+var numberListMediana = [];
+
+
+function agregarNumeroMediana() {
+  let number_input = document.getElementById('InputNumberMediana');
+  let number_value = number_input.value;
+
+  numberListMediana.push(parseInt(number_value));
+  number_input.value = '';
+  ordenarListaMediana();
+  
+  let lista_de_numeros = document.getElementById('NumberListMediana');
+  
+  lista_de_numeros.innerText = numberListMediana;
+}
+
+function ordenarListaMediana() {
+  numberListMediana.sort(function(a, b) {
+    return a - b;
+  });
+  return numberListMediana;
+}
+
+function medianaListEsPar() {
+  if (numberListMediana.length % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+function calcularMediana() {
+  ordenarListaMediana();
+  let mitadListMediana = parseInt(numberListMediana.length / 2);
+  if (!medianaListEsPar()) {
+    return numberListMediana[mitadListMediana];
+  } else {
+    let elemento1 = numberListMediana[mitadListMediana];
+    let elemento2 = numberListMediana[mitadListMediana - 1];
+
+    let promedio = calcularMediaAritmetica([elemento1, elemento2]);
+    return promedio;
+  }
+}
+
+function mostrarResultadoMediana() {
+  let resultado = calcularMediana();
+
+  let resultado_input_mediana = document.getElementById('ResultadoInputMediana');
+  resultado_input_mediana.innerText = 'La mediana es : ' + resultado;
+}
+
+
+function resetListMediana() {
+  while(numberListMediana.length > 0) {
+    numberListMediana.pop();
+  }
+
+  let lista_de_numeros = document.getElementById('NumberListMediana');
+  lista_de_numeros.innerText = numberListMediana;
+  document.getElementById('ResultadoInputMediana').innerText = '';
 }
