a1354201cc4509f307ee245e879c23dd1a4f6247
Simón A Azócar <saimn.azocar@gmail.com>
2021-10-04 14:11:15 -0400

    Codigo del promedio listo

--- promedio.js
+++ promedio.js
@@ -1,16 +1,48 @@
-function calcularMediaAritmetica(lista) {
-  // let sumaLista = 0;
-  // for (let i = 0; i < lista.length; i++) {
-  //   sumaLista = sumaLista + lista[i];
-  // }
+//  Codigo para el Promedio
 
-  const sumaLista = lista.reduce(
-    function (valorAcumulado = 0, nuevoElemento) {
-      return valorAcumulado + nuevoElemento;
-    }
-  );
+var numberListPromedio = [];
 
-  const promedioLista = sumaLista / lista.length;
+function calcularMediaAritmetica(numberListPromedio) {
+  let sumaNumberList = 0;
+
+  for (let i = 0; i < numberListPromedio.length; i++) {
+    sumaNumberList += numberListPromedio[i];
+  }
+
+  // const sumaLista = lista.reduce(
+  //   function (valorAcumulado = 0, nuevoElemento) {
+  //     return valorAcumulado + nuevoElemento;
+  //   }
+  // );
+
+  const promedioLista = sumaNumberList / numberListPromedio.length;
 
   return promedioLista;
+}
+
+function agregarNumeroPromedio() {
+  let number_input = document.getElementById('InputNumberPromedio');
+  let number_value = number_input.value;
+
+  numberListPromedio.push(parseInt(number_value));
+  number_input.value = '';
+  
+  let lista_de_numeros = document.getElementById('NumberListPromedio');
+  
+  lista_de_numeros.innerText = numberListPromedio;
+}
+
+function mostrarResultadoPromedio() {
+  let resultado_input = document.getElementById('ResultadoInputPromedio');
+  resultado_input.innerText = 'El promedio es: ' + calcularMediaAritmetica(numberListPromedio);
+}
+
+function resetListPromedio() {
+  while(numberListPromedio.length > 0) {
+    numberListPromedio.pop();
+  }
+
+  let lista_de_numeros = document.getElementById('NumberListPromedio');
+  lista_de_numeros.innerText = numberListPromedio;
+  document.getElementById('ResultadoInputPromedio').innerText = '';
 }
